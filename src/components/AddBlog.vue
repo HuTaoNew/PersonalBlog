cfe0a6dc5e59adc1f23b8f2f8feadf7fbcada187
Hutao <hutao_hn@163.com>
2018-12-24 00:58:40 +0800

    个人博客初始化建项目

---
+++ src/components/AddBlog.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="add-blog">
+      <h2>写博客</h2>
+      <form action="" method="post">
+          <div>
+              <span>主题：</span>
+              <input type="text" name="subject">
+          </div>
+          <div>
+              <span>内容：</span>
+              <textarea name="content" id="" cols="30" rows="10"></textarea>
+          </div>
+          <div>
+              <span>分类：</span>
+              <input type="checkbox" name="classify" value="vue">Vue.js
+              
+          </div>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'add-blog',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
