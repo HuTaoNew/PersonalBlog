1ab4d7090a0c0658d77ea943a6c8614d31525c80
Hutao <hutao_hn@163.com>
2018-12-28 00:41:51 +0800

    添加些博客功能

--- src/components/AddBlog.vue
+++ src/components/AddBlog.vue
@@ -1,36 +1,81 @@
 <template>
   <div class="add-blog">
-      <h2>写博客</h2>
-      <form action="" method="post">
-          <div>
-              <span>主题：</span>
-              <input type="text" name="subject">
-          </div>
-          <div>
-              <span>内容：</span>
-              <textarea name="content" id="" cols="30" rows="10"></textarea>
-          </div>
-          <div>
-              <span>分类：</span>
-              <input type="checkbox" name="classify" value="vue">Vue.js
-              
-          </div>
-      </form>
+    <h2>写博客</h2>
+    <form action method="post" v-if="!submitted">
+      <div>
+        <label>主题：</label>
+        <!-- required 属性规定必需在提交之前填写输入字段 -->
+        <input type="text" name="subject" v-model="blog.subject" required>
+      </div>
+      <div>
+        <label>内容：</label>
+        <textarea name="content" id cols="30" rows="10" v-model="blog.content"></textarea>
+      </div>
+      <div id="checkbox">
+        <label>分类：</label>
+        <input type="checkbox" name="classify" value="vue" v-model="blog.categories">Vue.js
+        <input type="checkbox" name="classify" value="angular4" v-model="blog.categories">Angular4
+        <input type="checkbox" name="classify" value="node" v-model="blog.categories">Node.js
+        <input type="checkbox" name="classify" value="react" v-model="blog.categories">React.js
+      </div>
+      <div>
+        <label>作者：</label>
+        <select v-model="blog.author">
+          <option v-for="(author, index) in authors" :key="index">{{author}}</option>
+        </select>
+      </div>
+      <button type="button" name="btnAddBlog" @click.prevent="addBlog()">添加博客</button>
+    </form>
+    <hr>
+
+    <h3 v-if="submitted">您的博客发送成功！！</h3>
+
+    <div id="prview">
+      <h3>博客总览</h3>
+      <p>博客标题：{{blog.subject}}</p>
+      <p>博客内容：</p>
+      <p>{{blog.content}}</p>
+      <p>博客分类：</p>
+      <ul>
+        <li v-for="(category, index) in blog.categories" :key="index">{{category}}</li>
+      </ul>
+      <p>作者：{{blog.author}}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'add-blog',
-  data () {
+  name: "add-blog",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      blog: {
+        subject: "",
+        content: "",
+        categories: [],
+        author: ""
+      },
+      authors: ["Mike", "Roso", "Jerry", "Tom", "Jason"],
+      submitted: false
+    };
+  },
+  methods: {
+    addBlog() {
+      this.$http
+        .post("https://jsonplaceholder.typicode.com/posts", {
+          title: this.blog.subject,
+          body: this.blog.content,
+          userId: 1
+        })
+        .then(data => {
+          console.log(data);
+          this.submitted = true;
+        });
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
